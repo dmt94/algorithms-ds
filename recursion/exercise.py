f2274c5cac6e9893faa91679c02c910c3eba3dfe
Daevah Taylaran <daevtayl@gmail.com>
2023-08-09 19:51:36 -0700

    sort exercises

--- recursion/exercise.py
+++ recursion/exercise.py
@@ -10,16 +10,50 @@ def greatest_product_of_3(arr):
 def find_missing_num(arr):
     arr.sort()
 
+    # array can have value that doesnt necessarily have to start at 0
+    # make sure end of range is the maximum known value + 1 bc range method is not inclusive 
     for i in range(arr[0], arr[-1] + 1):
         if i not in arr:
             print(i)
 
-# find_missing_num([2, 3, 4, 6, 7])
-
-
 find_missing_num([2,5,4,6,7])
 
-# print(len([2,3,4,6,7]))
-
 arrs = [3,5,4,6,7]
-print(arrs[-1] + 1)
+
+
+#3 Write 3 diff implementation of a function that finds the greatest number within an array. write one function that is O(N^2), one that is O(N log N), and one that is O(N)
+
+#NOTE O(N^2) Quadratic = nested for loops
+
+def find_max(arr):
+    for num in arr:
+        max_num = num
+        for compare_num in arr:
+            if compare_num > num:
+                max_num = compare_num
+    
+    return max_num
+
+print(find_max([4,3,2,44,1]))
+
+
+#NOTE O(N log N) Sort
+
+def find_max_sort(arr):
+    arr.sort()
+
+    return arr[-1]
+
+
+#NOTE O(N) Sort, loop once
+
+def find_max_loop(arr):
+    max_num = arr[0]
+
+    for num in arr:
+        if num > max_num:
+            max_num = num
+
+    return max_num
+
+print(find_max_loop([6,7,2,3,11,233,100,34]))
