62dc943b6a7f4c51edd755e117f2cb4e56a8efff
Daevah Taylaran <daevtayl@gmail.com>
2023-08-13 14:37:05 -0700

    linked list

--- linked-list/linkedlist.py
+++ linked-list/linkedlist.py
@@ -6,21 +6,27 @@
 #each node contains two main components:
   # actual data ('data')
   # reference to the next node ('next')
+
+# this class represents the individual elements or nodes in the linked list
+# each node has two attributes: 'data' to store the value of the node and 'next' to store a reference to the next node in the sequence
 class Node:
     def __init__(self, data):
-        # constructor, is a special method in a class that is used to initialize the attributes and state of an instance when it is created.
+        # constructor, is a special method in a class that is used to initialize the attributes and state of an instance when it is created. This is what initializes the attributes of 'data' and 'next' when a new node is created
         self.data = data
         self.next = None
+
+# class represents the linked list itself. It has a single attribute 'head', which is a reference to the first node in the linked list 
 
 class LinkedList:
     def __init__(self):
         self.head = None
+        # initially, the linked list is empty, so 'head' is set to 'None'
     
     def append(self, data):
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
-            return
+            return        
         current = self.head
         while current.next:
             current = current.next
