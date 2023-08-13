6999036762174e1f52679597ebe9d8e982c1009b
Daevah Taylaran <daevtayl@gmail.com>
2023-08-13 15:05:57 -0700

    singly linked list

--- linked-list/linkedlist.py
+++ linked-list/linkedlist.py
@@ -1,4 +1,4 @@
-# Creating a linked list as a class allows us to encapsulate the behavior and data associated with a linked list into a single entity.
+# Creating a SINGLY linked list as a class allows us to encapsulate the behavior and data associated with a linked list into a single entity.
 
 
 #NOTE We define a class called 'Node'
@@ -23,16 +23,27 @@ class LinkedList:
         # initially, the linked list is empty, so 'head' is set to 'None'
     
     def append(self, data):
+        # append method is used to add a new node with the given 'data' to the end of the linked list
+        
         new_node = Node(data)
+        # an instance of the Node class ( a new node ) is created, passing in data used in the argument to create the new node
         if self.head is None:
+            # if the self.head is None, it means that the linked list is empty. If this is the case, then the 'head' is set to the newly created node (new_node) and exits
             self.head = new_node
             return        
         current = self.head
+            # if the linked list IS NOT empty, a variable named current is created and set to the node that represents the 'head'
         while current.next:
+            # while loop traverses through the linked list using the 'current' variable. It starts from the 'head' and moves to the last node by following the 'next' references until it reaches the last node. 'While current.next' means that while current.next is not equal to None, the loop keeps going because the last node isn't reached yet since only the last node will have the 'next' reference as 'None'.
             current = current.next
         current.next = new_node
+        #once the last node has been reached, it updates the 'next' reference of the last node to point to the newly created node, effectively adding it to the end of the linked list
+
+        #NOTE appending = adding to the linked list, so you are finding the last node to attach the new node as the 'next' reference to that last node
+
     
     def display(self):
+        #method is used to print the elements of the linekd list in order. it starts from the 'head' and iterates through the linked list by following the 'next' references. 
         current = self.head
         while current:
             print(current.data, end=" -> ")
